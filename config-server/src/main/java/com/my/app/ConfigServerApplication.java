1bfdb69b91beb73987976ebd777707c43ce2130b
ArtsiomBarodka <artsiombarodka@gmail.com>
2022-04-21 15:26:00 +0300

    added config-server

--- config-server/src/main/java/com/my/app/ConfigServerApplication.java
+++ config-server/src/main/java/com/my/app/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.my.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class Application {
+public class ConfigServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ConfigServerApplication.class);
     }
 }
--- order/src/main/java/com/my/app/controller/OrderController.java
+++ order/src/main/java/com/my/app/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.my.app.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrderController {
+    @Value("${app.service.name}")
+    private String name;
 
     @GetMapping
     public String healthCheck(){
-        return "Hello from order";
+        return "Hello from " + name;
     }
 }
