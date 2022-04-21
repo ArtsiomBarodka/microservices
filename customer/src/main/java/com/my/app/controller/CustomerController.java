d5ef1ec5753dc29a2b69105c8c3746da81c82bc5
ArtsiomBarodka <artsiombarodka@gmail.com>
2022-04-21 13:29:19 +0300

    added two simple applications (Customer and Order)

---
+++ customer/src/main/java/com/my/app/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package com.my.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+
+    @GetMapping
+    public String healthCheck(){
+        return "Hello from customer";
+    }
+}
---
+++ order/src/main/java/com/my/app/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.my.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+public class OrderController {
+
+    @GetMapping
+    public String healthCheck(){
+        return "Hello from order";
+    }
+}
