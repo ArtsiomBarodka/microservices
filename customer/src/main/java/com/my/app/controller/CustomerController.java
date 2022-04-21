d851294fa532ffeab67f858862492e9269506176
ArtsiomBarodka <artsiombarodka@gmail.com>
2022-04-21 16:09:17 +0300

    added refresh-scope and actuator for updating parameters via actuator/refresh

--- customer/src/main/java/com/my/app/controller/CustomerController.java
+++ customer/src/main/java/com/my/app/controller/CustomerController.java
@@ -1,10 +1,12 @@
 package com.my.app.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/api/v1/customers")
 public class CustomerController {
