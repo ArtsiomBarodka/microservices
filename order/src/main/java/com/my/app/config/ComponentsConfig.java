1f890492cc405d9432501f5e92d25822c755bc5d
Artsiom_Barodka <artsiombarodka@gmail.com>
2022-12-22 15:54:18 +0200

    fixed load balancer issue with docker without spring discovery

--- order/src/main/java/com/my/app/config/ComponentsConfig.java
+++ order/src/main/java/com/my/app/config/ComponentsConfig.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -14,8 +15,15 @@ public class ComponentsConfig {
     }
 
     @Bean
+    @Profile("!withoutLoadBalancer")
     @LoadBalanced
     public WebClient.Builder builder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    @Profile("withoutLoadBalancer")
+    public WebClient.Builder builderWithoutLoadBalancer() {
         return WebClient.builder();
     }
 
